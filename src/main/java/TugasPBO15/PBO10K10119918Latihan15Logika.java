f07b0347facd9c446afa2b5c9982a0d264e86fa7
Andreas Suryadi <andreas.suryadi02@gmail.com>
2020-10-21 08:40:47 +0700

    PBO10K-10119918-Latihan15-Logika

---
+++ src/main/java/TugasPBO15/PBO10K10119918Latihan15Logika.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasPBO15;
+
+/**
+ *
+ * @author
+ * NAMA     : Andreas Suryadi
+ * KELAS    : PBO10K
+ * NIM      : 10119918
+ * Deskripsi Program : Program ini berisi program untuk menampilkan hasil 
+ * operator logika
+ * 
+ */
+public class PBO10K10119918Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + (!a));
+        System.out.println("!(a && b )= " + !(a && b));
+    }
+    
+}
